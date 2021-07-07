0a040dba065ae6aa66b54637b77fea8d121a4076
Douglas Lopes <douglasdmlopes@gmail.com>
2021-07-06 22:24:39 -0300

    start

---
+++ pages/cataline/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{$route.params}}
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
