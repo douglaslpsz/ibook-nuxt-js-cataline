8a181c562de50fbbb96ad73bb52c9705b33772b6
Douglas Lopes <douglasdmlopes@gmail.com>
2021-07-11 16:53:48 -0300

    Estruturas e layouts iBook

---
+++ components/organisms/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer class="footer">
+    <Container>
+      <Logo />
+      <p>Copyring &copy; {{currentYear}} iBook</p>
+    </Container>
+  </footer>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend ({
+  computed: {
+    currentYear() {
+      return new Date().getFullYear()
+    }
+  }  
+})
+
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  width: 100%;
+  height: 110px;
+  background: color(dark, darkest);
+  .container {
+    display: grid;
+    height: 100%;
+    grid-auto-flow: column;
+    justify-content: space-between;
+    align-items: center;
+    p {
+      color: color('light', 'darkest');
+    }
+  }
+}
+</style>
